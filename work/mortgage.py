3701a9d27e952dee1630a188f6624e33b6a0fd8f
Dajebbar <dajebbar@gmx.fr>
2021-10-14 19:41:54 +0100

    mortgage.py

---
+++ work/mortgage.py
@@ -0,0 +1,14 @@
+def mortgage(principal, rate, payment):
+    total_paid = 0.0
+    while principal > 0:
+        principal = principal * (1+rate/12) - payment
+        total_paid += payment
+    return total_paid
+
+
+principal = 500000
+rate = 0.05
+payment = 2684.11
+
+total_paid = mortgage(principal, rate, payment)
+print('Total paid', total_paid)
