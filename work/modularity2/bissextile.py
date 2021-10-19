7b1baf2f319f295e63cefe3821ac9a0f8583f02b
Dajebbar <dajebbar@gmx.fr>
2021-10-19 10:07:10 +0100

    Assertion raise

--- work/modularity2/bissextile.py
+++ work/modularity2/bissextile.py
@@ -1,5 +1,4 @@
 def bissextile(year):
-    year_checked(year)
     res = ""
     if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
         res = f"{year} is a leap year."
@@ -9,26 +8,25 @@ def bissextile(year):
     return res
 
 
-def year_checked(year):
-    if len(str(year)) < 4:
-        print(f"{year} is not a valid year.")
+if __name__ == "__main__":
+    go = True
+
+    while go:
+
+        try:
+            year = int(input("Give a valid year >>> "))
+            if year <= 0 or len(str(year)) != 4:
+                raise ValueError(
+                    "Year should be positive and contains 4 digits")
+
+        except ValueError as ve:
+            print(ve)
 
 
-go = True
+        else:
 
+            print(bissextile(year))
 
-while go:
-
-    try:
-        year = int(input("Give a valid year >>> "))
-
-    except ValueError as ve:
-        print(ve)
-
-    else:
-
-        print(bissextile(year))
-
-    res = input("Would you like to continue??[y/n] >>> ")
-    if res == 'n' or res == 'N':
-        go = False
+        res = input("Would you like to continue??[y/n] >>> ")
+        if res == 'n' or res == 'N':
+            go = False
