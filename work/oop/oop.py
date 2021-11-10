82d85b03b957453f7cba399b3cd53e888f2f5051
Dajebbar <dajebbar@gmx.fr>
2021-11-10 18:04:12 +0100

    classmethod alternative constructor

--- work/oop/oop.py
+++ work/oop/oop.py
@@ -14,6 +14,15 @@ class BasketBallTeam:
         self.total_fines += self.fine_amount  # BasketBallTeam.fine_amount
         # return self.total_fines
 
+    @classmethod
+    def set_fines(cls, amount):
+        cls.fine_amount = amount
+
+    @classmethod
+    def from_string(cls, stats_as_str):
+        team, win, loss = stats_as_str.strip().split("-")
+        return cls(team, int(win), int(loss))
+
     def stat(self):
         return f"[BASKETBALL] STATS: {self.team}: {BasketBallTeam.pluralize(self.wins, 'victory', 'victories')} - {BasketBallTeam.pluralize(self.losses, 'defeat')}"
 
@@ -29,34 +38,57 @@ class BasketBallTeam:
 team_1 = BasketBallTeam("Los Ageles lakers", 12, 8)
 team_2 = BasketBallTeam("Chicago Bulls", 10, 0)
 
-print(team_1.team)
-print(team_2.team)
+# print(team_1.team)
+# print(team_2.team)
 
-print(team_1.wins, team_1.losses)
-print(team_2.wins, team_2.losses)
+# print(team_1.wins, team_1.losses)
+# print(team_2.wins, team_2.losses)
 
-print(team_1.stat())
-print("=*=" * 20)
-print(team_2.stat())
+# print(team_1.stat())
+# print("=*=" * 20)
+# print(team_2.stat())
 
-team_1.get_fined()
-team_1.get_fined()
-print(team_1.total_fines)
+# team_1.get_fined()
+# team_1.get_fined()
+# print(team_1.total_fines)
 
-print(vars(BasketBallTeam))
-print()
-print("=*=" * 30)
-print()
-print(vars(team_1))
+# print(vars(BasketBallTeam))
+# print()
+# print("=*=" * 30)
+# print()
+# print(vars(team_1))
 
-BasketBallTeam.fine_amount = 30_000
-print(team_2.fine_amount)
-team_1.fine_amount = 18_000
-print(BasketBallTeam.fine_amount)
-print(team_1.fine_amount)
-print(team_2.fine_amount)
+# BasketBallTeam.fine_amount = 30_000
+# print(team_2.fine_amount)
+# team_1.fine_amount = 18_000
+# print(BasketBallTeam.fine_amount)
+# print(team_1.fine_amount)
+# print(team_2.fine_amount)
 
-print(vars(team_1))
-print(vars(team_2))
+# print(vars(team_1))
+# print(vars(team_2))
 
-print(BasketBallTeam.team_cnt)
+# print(BasketBallTeam.team_cnt)
+
+# BasketBallTeam.set_fines(95_000)
+# print(team_1.fine_amount)
+# print(team_2.fine_amount)
+# team_1.set_fines(61_000)
+# print(team_1.fine_amount)
+# print(team_2.fine_amount)
+# print(BasketBallTeam.fine_amount)
+# print(vars(team_2))
+# print(vars(BasketBallTeam))
+
+# utah_stats = "Utah Jazz-34-7"
+# team, win, loss = utah_stats.split("-")
+# team_3 = BasketBallTeam(team, int(win), int(loss))
+# team_3 = BasketBallTeam.from_string(utah_stats)
+# print(team_3.stat())
+
+
+with open('nba.txt', 'r', encoding='utf-8') as f:
+
+    for line in f:
+        stat = BasketBallTeam.from_string(line)
+        print(stat.stat())
