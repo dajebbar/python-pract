ef9ca7d2da9ae1ef209c74f0f23ff3f2d43d7c50
Dajebbar <dajebbar@gmx.fr>
2021-11-11 16:19:51 +0100

    team_cnt refact

--- work/oop/oop_inherit.py
+++ work/oop/oop_inherit.py
@@ -11,6 +11,14 @@ class SportTeam:
         self.wins = wins
         self.losses = losses
         self.total_fines = 0
+
+        if self.__class__.__name__ == 'BasketBallTeam':
+            BasketBallTeam.team_cnt += 1
+        elif self.__class__.__name__ == 'FootBallTeam':
+            FootBallTeam.team_cnt += 1
+        elif self.__class__.__name__ == 'BaseBallTeam':
+            BaseBallTeam.team_cnt += 1
+
         SportTeam.team_cnt += 1
 
     def get_fined(self):
@@ -18,7 +26,7 @@ class SportTeam:
         # return self.total_fines
 
     def stat(self):
-        return f"{self.team}: {utils.pluralize(self.wins, 'victory', 'victories')} - {utils.pluralize(self.losses, 'defeat')}"
+        return f"{self.team} has {utils.pluralize(self.wins, 'victory', 'victories')} and {utils.pluralize(self.losses, 'defeat')}"
 
     @classmethod
     def set_fines(cls, amount):
@@ -40,18 +48,26 @@ class SportTeam:
 
 
 class BasketBallTeam(SportTeam):
+    # def __init__(self, team_name, wins, losses):
+    #     super().__init__(team_name, wins, losses)
+    team_cnt = 0
+
     def stat(self):
-        return "[Basketball stats]: " + super().stat()
+        return f"[Basketball stats]: {super().stat()}"
 
 
 class FootBallTeam(SportTeam):
+    team_cnt = 0
+
     def stat(self):
-        return "[Foottball stats]: " + super().stat()
+        return f"[Foottball stats]: {super().stat()}"
 
 
 class BaseBallTeam(SportTeam):
+    team_cnt = 0
+
     def stat(self):
-        return "[Baseball stats]: " + super().stat()
+        return f"[Baseball stats]: {super().stat()}"
 
 # basket_team_1 = BasketBallTeam("Los Ageles lakers", 12, 8)
 # basket_team_2 = BasketBallTeam("Chicago Bulls", 10, 0)
@@ -69,7 +85,7 @@ for team in basket_team_4:
     print(team.stat())
 
 
-print("==*==" * 10)
+print("==*==" * 15)
 
 foot_team_1 = FootBallTeam.from_file('nfl.txt')
 for team in foot_team_1:
@@ -81,3 +97,23 @@ print("==*==" * 15)
 baseball_team_1 = BaseBallTeam.from_file('baseball.txt')
 for team in baseball_team_1:
     print(team.stat())
+
+
+# basket_team_4 = BasketBallTeam.from_file('nba.txt')
+# basket_team_4[0].get_fined()
+# team_name = basket_team_4[0].team
+# team_wins = basket_team_4[0].wins
+# team_losses = basket_team_4[0].losses
+
+# print(team_name, team_wins, team_losses)
+
+# team_fines = basket_team_4[0].total_fines
+# print(team_fines)
+# print(vars(basket_team_4[0]))
+# print(basket_team_4[0].fine_amount)
+# print(help(basket_team_4[0]))
+
+print(f"number of sport teams: {SportTeam.team_cnt}")
+print(f"number of basket teams: {BasketBallTeam.team_cnt, len(basket_team_4)}")
+print(f"number of foot teams: {FootBallTeam.team_cnt, len(foot_team_1)}")
+print(f"number of base teams: {BaseBallTeam.team_cnt, len(baseball_team_1)}")
