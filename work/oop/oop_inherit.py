301cea8a2f4ac6915117a94c5361369e1c642c30
Dajebbar <dajebbar@gmx.fr>
2021-11-11 17:41:27 +0100

    refactoring class methods

--- work/oop/oop_inherit.py
+++ work/oop/oop_inherit.py
@@ -19,12 +19,14 @@ class SportTeam:
         # elif self.__class__.__name__ == 'BaseBallTeam':
         #     BaseBallTeam.team_cnt += 1
 
-        if isinstance(self, BasketBallTeam):
-            BasketBallTeam.team_cnt += 1
-        elif isinstance(self, FootBallTeam):
-            FootBallTeam.team_cnt += 1
-        elif isinstance(self, BaseBallTeam):
-            BaseBallTeam.team_cnt += 1
+        # if isinstance(self, BasketBallTeam):
+        #     BasketBallTeam.team_cnt += 1
+        # elif isinstance(self, FootBallTeam):
+        #     FootBallTeam.team_cnt += 1
+        # elif isinstance(self, BaseBallTeam):
+        #     BaseBallTeam.team_cnt += 1
+
+        self.__class__.team_cnt += 1
 
         SportTeam.team_cnt += 1
 
@@ -40,17 +42,27 @@ class SportTeam:
         cls.fine_amount = amount
 
     @classmethod
-    def from_string(cls, stats_as_str):
-        team, win, loss = stats_as_str.strip().split("-")
-        return cls(team, int(win), int(loss))
+    def from_string(cls, stats_as_str, draws=0):
+        if draws:
+            team, win, loss, draws = stats_as_str.strip().split("-")
+            return cls(team, int(win), int(loss), int(draws))
+
+        else:
+            team, win, loss = stats_as_str.strip().split("-")
+            return cls(team, int(win), int(loss))
 
     @classmethod
-    def from_file(cls, link):
+    def from_file(cls, link, draws=0):
         basket_objects = []
         with open(link, 'r', encoding='utf-8') as f:
             for line in f:
-                team, win, loss = line.strip().split("-")
-                basket_objects.append(cls(team, int(win), int(loss)))
+                if draws:
+                    team, win, loss, draws = line.strip().split("-")
+                    basket_objects.append(
+                        cls(team, int(win), int(loss), int(draws)))
+                else:
+                    team, win, loss = line.strip().split("-")
+                    basket_objects.append(cls(team, int(win), int(loss)))
         return basket_objects
 
 
@@ -58,6 +70,7 @@ class BasketBallTeam(SportTeam):
     # def __init__(self, team_name, wins, losses):
     #     super().__init__(team_name, wins, losses)
     team_cnt = 0
+    fine_amount = 100_000
 
     def stat(self):
         return f"[Basketball stats]: {super().stat()}"
@@ -65,9 +78,14 @@ class BasketBallTeam(SportTeam):
 
 class FootBallTeam(SportTeam):
     team_cnt = 0
+    fine_amount = 20_000
+
+    def __init__(self, team_name, wins, losses, draws):
+        super().__init__(team_name, wins, losses)
+        self.draws = draws
 
     def stat(self):
-        return f"[Foottball stats]: {super().stat()}"
+        return f"[Foottball stats]: {super().stat()} {utils.pluralize(self.draws, 'draw')}"
 
 
 class BaseBallTeam(SportTeam):
@@ -92,9 +110,13 @@ for team in basket_team_4:
     print(team.stat())
 
 
+# print(basket_team_4[1].total_fines)
+# basket_team_4[1].get_fined()
+# print(f"{basket_team_4[1].team} has a total fines of ${basket_team_4[1].total_fines}")
+
 print("==*==" * 15)
 
-foot_team_1 = FootBallTeam.from_file('nfl.txt')
+foot_team_1 = FootBallTeam.from_file('nfl.txt', draws='yes')
 for team in foot_team_1:
     print(team.stat())
 
@@ -104,6 +126,10 @@ print("==*==" * 15)
 baseball_team_1 = BaseBallTeam.from_file('baseball.txt')
 for team in baseball_team_1:
     print(team.stat())
+
+# print(baseball_team_1[-1].total_fines)
+# baseball_team_1[-1].get_fined()
+# print(f"{baseball_team_1[-1].team} has a total fines of ${baseball_team_1[-1].total_fines}")
 
 
 # basket_team_4 = BasketBallTeam.from_file('nba.txt')
@@ -120,7 +146,7 @@ for team in baseball_team_1:
 # print(basket_team_4[0].fine_amount)
 # print(help(basket_team_4[0]))
 
-print(f"number of sport teams: {SportTeam.team_cnt}")
-print(f"number of basket teams: {BasketBallTeam.team_cnt, len(basket_team_4)}")
-print(f"number of foot teams: {FootBallTeam.team_cnt, len(foot_team_1)}")
-print(f"number of base teams: {BaseBallTeam.team_cnt, len(baseball_team_1)}")
+# print(f"number of sport teams: {SportTeam.team_cnt}")
+# print(f"number of basket teams: {BasketBallTeam.team_cnt}")
+# print(f"number of foot teams: {FootBallTeam.team_cnt}")
+# print(f"number of base teams: {BaseBallTeam.team_cnt}")
