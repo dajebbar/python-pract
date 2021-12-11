53202f62d7c49f9782c3cb324746e4beaf6cdebc
Dajebbar <dajebbar@gmx.fr>
2021-12-11 11:55:41 +0100

    Brush options and slider

--- work/guis/projects_gui/paint/paint_app.py
+++ work/guis/projects_gui/paint/paint_app.py
@@ -1,4 +1,5 @@
 import sys
+import tkinter.ttk as ttk
 from tkinter import *
 
 # Paint Function
@@ -8,8 +9,9 @@ def paint(e):
     ''''''
     # Brush Parameters
     brush_width = 20
-    brush_color = 'light green'
-    brush_type = BUTT
+    brush_color = 'green'
+    # BRUSH TYPE: BUTT, ROUND, PROJECTING
+    brush_type = PROJECTING
 
     # Srating Position
     x1.set(e.x - 1)
@@ -23,10 +25,16 @@ def paint(e):
     canvas.create_line(x1.get(), y1.get(), x2.get(), y2.get(),
                        fill=brush_color, width=brush_width, capstyle=brush_type, smooth=True)
 
+# change brush size Function
+
+
+def change_brush_size():
+    ''''''
+
 
 root = Tk()
 root.title('Funny Painter Paint')
-root.geometry('800x600')
+root.geometry('800x800')
 if sys.platform.startswith('win'):
     root.iconbitmap('img/infinity.ico')
 else:
@@ -34,15 +42,30 @@ else:
     root.call('wm', 'iconphoto', root._w, icon)
 
 # Variables
+w = 600
+h = 400
 x1 = IntVar()
 y1 = IntVar()
 x2 = IntVar()
 y2 = IntVar()
 
 # Adding Canvas
-canvas = Canvas(root, width=500, height=400, bg='light grey')
+canvas = Canvas(root, width=w, height=h, bg='white')
 canvas.bind('<B1-Motion>', paint)
 canvas.pack(pady=20)
+
+# Create Brush Options Frame
+brush_options_frame = Frame(root)
+brush_options_frame.pack(pady=20)
+
+# Brush Size
+brush_size_frame = LabelFrame(brush_options_frame, text='Brush Size')
+brush_size_frame.grid(row=0, column=0, padx=50)
+
+# Brush Slider
+my_slider = ttk.Scale(brush_size_frame, from_=1, to=100,
+                      command=change_brush_size, orient=VERTICAL, value=10)
+my_slider.pack(padx=10, pady=10)
 
 
 root.mainloop()
