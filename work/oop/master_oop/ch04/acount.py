80b9dd1dc8a6eb09300981e72bfbd49cb77dbadf
Dajebbar <dajebbar@gmx.fr>
2021-12-21 11:29:23 +0100

    account modified

---
+++ work/oop/master_oop/ch04/acount.py
@@ -0,0 +1,55 @@
+class Account:
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = int(balance)
+        self.password = password
+    
+    def deposit(self, amount_to_deposit, password):
+        if password != self.password:
+            print('Sorry, incorrect password')
+            return None
+        
+        if amount_to_deposit < 0:
+            print('You cannot deposit a negative amount')
+            return None
+        
+        self.balance += amount_to_deposit
+        print('done!')
+        return self.balance
+    
+    def withdraw(self, amount_to_withdraw, password):
+        if password != self.password:
+            print('Incorrect password for this account')
+            return None
+        if amount_to_withdraw < 0:
+            print('You cannot withdraw a negative amount')
+            return None
+        if amount_to_withdraw > self.balance:
+            print('You cannot withdraw more than you have in your account')
+            return None
+        
+        self.balance -= amount_to_withdraw
+        print('done!')
+        return self.balance
+    
+    def get_balance(self, password):
+        if password != self.password:
+            print('Sorry, incorrect password')
+            return None
+        return self.balance
+    
+    # Added for debugging
+    def show(self):
+        print('       Name:', self.name)
+        print('       Balance:', self.balance)
+        print('       Password:', self.password)
+        print()
+
+
+def main():
+    account1 =  Account('Joe Schmoe', 1000, 'magic')
+    new_deposit = account1.deposit(500, 'magic')
+    account1.show()
+
+if __name__=='__main__':
+    main()
