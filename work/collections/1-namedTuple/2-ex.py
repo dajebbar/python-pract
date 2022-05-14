7ebc4b995b4872cf2c383ce1b4015572a1f2b420
Dajebbar <dajebbar@gmx.fr>
2022-05-14 13:20:54 +0100

    looping into l

--- work/collections/1-namedTuple/2-ex.py
+++ work/collections/1-namedTuple/2-ex.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 class Person:
     def __init__(self, fname, lname, age):
         self.fname = fname
@@ -15,8 +17,20 @@ class Trainee(Person):
         self.role = role
     
     def __str__(self):
-        print(f'{super().__str__()}\nID: {self.id}\nRole: {self.role}')
+        return f'{super().__str__()}\nID: {self.id}\nRole: {self.role}'
 
 
 t1 = Trainee('ali', 'boubek', '20', '45-rt123', 'DLE')
-t1
+p1 = Person('fz', 'lx', '15')
+p2 = Person('krm', 'xml', 25)
+# print(t1)
+
+L = namedtuple('Per_train', ['Per', 'Train'])
+l = L((p1, p2), t1)
+# print(l)
+
+for p in l.Per:
+    print(p)
+    print('-**-'* 5)
+
+print(l.Train)
