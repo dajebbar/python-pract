af7b2be1aaf84e2187c1efc95cea5d00416cdb89
Dajebbar <dajebbar@gmx.fr>
2022-05-17 12:46:44 +0100

    csv

--- work/collections/project_1/vendors.py
+++ work/collections/project_1/vendors.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from p1 import Vendor
 from collections import deque
 import csv
@@ -6,6 +7,7 @@ from os import strerror
 def get_vendor():
     print('--*** VENDORS ***--')
     n_vendor = int(input('Number of vendor(s)?>>> '))
+    # data rows of csv file
     vend = deque()
     for item in range(n_vendor):
         print(f'--- INFO VENDOR {item+1} ---')
@@ -17,18 +19,20 @@ def get_vendor():
 
         vend.append(v.vendors())
     
-    print(vend)
-
+    # print(vend)
     
+    # field names
+    fields = ['code_vendor', 'adress', 'commercial_name',]
+    # name of csv file
+    filename = "vendor_file.csv"
     
-    # try:
-    #     with open('vendor_file.csv', 'a') as f:
-            
-    #         for elem in vendors:
-    #             writer = csv.writer(f)
-    #             writer.writerow(elem)
-    # except Exception as e:
-    #     print(strerror(e.erno))
+    try:
+        with open(filename, 'a') as f:
+            csvwriter = csv.writer(f)
+            # csvwriter.writerow(fields)
+            csvwriter.writerows(vend)
+    except Exception as e:
+        print(strerror(e.erno))
 
 
 if __name__=='__main__':
