a416190a20a2332ab3e9d74b78d15449c25e0243
Dajebbar <dajebbar@gmx.fr>
2022-05-17 15:20:28 +0100

    take file

--- work/collections/project_2/p2.py
+++ work/collections/project_2/p2.py
@@ -1,5 +1,6 @@
 def take_file():
     v = []
+    l = {}
     file_name = input('file link?>> ')
     with open(file_name, 'r') as f:
         for line in f.readlines():
@@ -7,16 +8,18 @@ def take_file():
                 print(chr, end='')
             
             v.append(line.split(' '))
-    return v
-                
-v = take_file()
-l = {}
-for item in v:
-    l[f'{item[-3]} {item[-2]}'] = float(item[-1][:-1])
+    
+    for item in v:
+        l[f'{item[-3]} {item[-2]}'] = float(item[-1][:-1])
 
-for k, v in l.items():
-    if l[k] == max(l.values()):
-        print(f'{k} has the max mark')
+    for k, v in l.items():
+        if l[k] == max(l.values()):
+            print(f'{k} has the max mark')
+                
+
+take_file()
+
+
 
 
 # print(l)
