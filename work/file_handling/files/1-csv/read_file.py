19be5084f6458666ef5382b02dd68f58ea5e76fa
Dajebbar <dajebbar@gmx.fr>
2022-05-18 12:52:09 +0100

    dictReader

--- work/file_handling/files/1-csv/read_file.py
+++ work/file_handling/files/1-csv/read_file.py
@@ -6,6 +6,13 @@ def read_files(filename):
         for line in reader_csv:
             print(line)
 
+def read_dict(filename):
+    with open(filename) as f:
+        reader_csv = csv.DictReader(f)
+        for line in reader_csv:
+            print(line)
+
 
 filename='combats.csv'
-read_files(filename)
+# read_files(filename)
+read_dict(filename)
