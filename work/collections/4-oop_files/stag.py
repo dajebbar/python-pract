3765a24c81df438f29e449973ea79571d30301bf
Dajebbar <dajebbar@gmx.fr>
2022-05-18 08:51:16 +0100

    read txt file

--- work/collections/4-oop_files/stag.py
+++ work/collections/4-oop_files/stag.py
@@ -33,21 +33,28 @@ class Trainee:
             f.write(self.__str__())
         print('done!')
 
+def display_files(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            for chr in line:
+                print(chr, end='')
 
 if __name__=='__main__':
-    # s1 = Trainee('id1111', 'alan', 'tores', 12.75)
-    # s1.save_to_file('trainees.txt')
-    with open('trainees.txt', 'r') as f:
-        v = []
-        for line in f.readlines():
-            v.append(line.split(' '))
+    # s1 = Trainee('id1111', 'alan', 'tores', 15.5)
+    # s1.save_to_file('tr.txt')
+
+    display_files('trainees.txt')
+    # with open('trainees.txt', 'r') as f:
+    #     v = []
+    #     for line in f.readlines():
+    #         v.append(line.split(' '))
         
     
-    for index in v:
-        for item in index:
-            if float(index[-1][index[-1].index(':')+1: -1]) >= 10:
-                with open('succes.txt', 'a') as f:
-                    f.write(item.split(':')[1]+ ' ')
-            else:
-                with open('failed.txt', 'a') as f:
-                    f.write(item.split(':')[1]+ ' ')
+    # for index in v:
+    #     for item in index:
+    #         if float(index[-1][index[-1].index(':')+1: -1]) >= 10:
+    #             with open('succes.txt', 'a') as f:
+    #                 f.write(item.split(':')[1]+ ' ')
+    #         else:
+    #             with open('failed.txt', 'a') as f:
+    #                 f.write(item.split(':')[1]+ ' ')
