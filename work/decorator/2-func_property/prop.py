f72459145f915b378058b3d199398f6405605ccf
Dajebbar <dajebbar@gmx.fr>
2022-05-18 11:22:05 +0100

    setter & getter

--- work/decorator/2-func_property/prop.py
+++ work/decorator/2-func_property/prop.py
@@ -4,5 +4,21 @@ class Employee:
         self.__dept = dept
         self.__city = city
     
+    def get_code(self):
+        return self.__code
+    def set_code(self, x):
+        self.__code=x
+    
+    def get_dept(self):
+            return self.__dept
+    def set_dept(self, x):
+        self.__dept=x
+    
+    def get_city(self):
+            return self.__city
+    def set_city(self, x):
+        self.__city=x
+
+
     def __str__(self):
         print(f'EmployeeID: {self.__code}\nDepartment: {self.__dept}\nCity: {self.__city}')
