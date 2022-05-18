f1ec9ae3ac4a528f5767e9bdc2a8da921677a685
Dajebbar <dajebbar@gmx.fr>
2022-05-18 12:59:22 +0100

    csv writer writerow

---
+++ work/file_handling/files/1-csv/write_file.py
@@ -0,0 +1,12 @@
+import csv
+
+
+def write_files(filename):
+    with open(filename, 'a') as f:
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerow(['fname', 'lname', 'mark'])
+        csv_writer.writerow(['ahmadi', 'alaa', '15.5'])
+        csv_writer.writerow(['khalidi', 'hind', 11])
+        csv_writer.writerow(['ibrahimi', 'layla', 16])
+    
+    print('Done!')
